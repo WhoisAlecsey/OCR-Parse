8fdc8980b062e9ac7045e5c1078868d6e701294c
whoisalecsey <whoisalecsey@gmail.com>
2018-11-17 15:51:28 +0300

    Add the simple menu.

--- ocr.py
+++ ocr.py
@@ -1,16 +1,19 @@
 from PIL import Image
 import pytesseract
 
+photo = "images/example_01.png"
+lang = "eng"
 
-def parsePhoto(photo = "example_01.png", lang = "eng"):
+def parsePhoto(photo="example_01.png", lang="rus"):
     f = open('text_photo.txt', 'w')
     text = pytesseract.image_to_string(Image.open(str(photo)), lang=lang)
     f.write(str(text.encode('utf-8')))
     print("\n%s" % str(text.encode('utf-8')))
 
+print("Write parsing language:\neng or rus")
+lang = raw_input()
+print("Enter path to photo:\n\tFor Example:\n\timages/example_01.png")
 
-print("Enter photo name with type and lang:\n\tFor Example:\n\texample_01.png\n\teng")
-photo = "images/%s" % raw_input()
-lang = "%s" % raw_input()
+photo = "%s" % raw_input()
 
 parsePhoto(photo, lang)
