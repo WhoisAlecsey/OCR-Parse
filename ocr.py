e54773846ad58696ac49fa924eb9b9d20152bf4e
whoisalecsey <whoisalecsey@gmail.com>
2018-12-02 17:07:32 +0300

    Fix bugs. Stable version

--- ocr.py
+++ ocr.py
@@ -4,16 +4,22 @@ import pytesseract
 photo = "images/example_01.png"
 lang = "eng"
 
+
 def parsePhoto(photo="example_01.png", lang="rus"):
     f = open('text_photo.txt', 'w')
     text = pytesseract.image_to_string(Image.open(str(photo)), lang=lang)
     f.write(str(text.encode('utf-8')))
     print("\n%s" % str(text.encode('utf-8')))
 
-print("Write parsing language:\neng or rus")
-lang = raw_input()
-print("Enter path to photo:\n\tFor Example:\n\timages/example_01.png")
 
-photo = "%s" % raw_input()
+while True:
+    lang = raw_input("\nWrite parsing language eng or rus: ")
+    if lang.strip() == 'eng' or lang.strip() == 'rus':
+        break
+
+while True:
+    photo = raw_input("Enter path to photo (e.g. images/example_01.png): ")
+    if photo[-4:] == '.png' or photo[-4:] == '.jpg':
+        break
 
 parsePhoto(photo, lang)
